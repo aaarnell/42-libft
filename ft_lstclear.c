2fb5630312898eb751168ae9d9f4b755ce0bfd65
Arachne Arnell <aarnell@mi-a5.msk.21-school.ru>
2021-09-01 21:30:05 +0300

    Added func. ft_lstclear and corrected to Norme func. ft_lstlast

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/31 22:32:04 by aarnell           #+#    #+#             */
-/*   Updated: 2021/09/01 21:28:30 by aarnell          ###   ########.fr       */
+/*   Created: 2021/09/01 21:04:13 by aarnell           #+#    #+#             */
+/*   Updated: 2021/09/01 21:28:50 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*cur;
+	t_list	*tmp;
 
-	cur = lst;
-	while (cur->next)
-		cur = cur->next;
-	return (cur);
+	if (lst && *lst && del)
+	{
+		cur = *lst;
+		while (cur)
+		{
+			tmp = cur->next;
+			ft_lstdelone(cur, del);
+			cur = tmp;
+		}
+		*lst = NULL;
+	}
 }
