51f1abdc8284b00d03b7fb61d0cd95da8a657b22
Arachne Arnell <aarnell@il-f5.msk.21-school.ru>
2021-09-04 13:50:31 +0300

    Func. ft_lstsize didn't count items. Fixed

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:23:03 by aarnell           #+#    #+#             */
-/*   Updated: 2021/08/31 22:31:19 by aarnell          ###   ########.fr       */
+/*   Updated: 2021/09/04 13:46:40 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	ft_lstsize(t_list *lst)
 	int	res;
 
 	res = 0;
-	while (lst && res++)
+	while (lst)
+	{
 		lst = lst->next;
+		res++;
+	}
 	return (res);
 }
